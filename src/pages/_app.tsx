19d408c777eea45ee160e2010d4734dcb6353b2f
Wade Austin <wade@few.io>
2023-02-17 12:49:25 -0600

    add theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
+import { ThemeProvider } from "@mui/material";
+import { theme } from "../styles/theme";
+
 import type { AppProps } from 'next/app'
+import CssBaseline from "@mui/material/CssBaseline";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,23 +3,11 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import { Button } from '@mui/material'
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
+import { Switch } from '@mui/material'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const label = { inputProps: { "aria-label": "Switch demo" } };
 
 export default function Home() {
   return (
@@ -30,57 +18,18 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+
+      <main className={styles.container}>
         <Button sx={{ marginBottom: 2 }} variant="contained">
           Button
         </Button>
 
-        <Card sx={{ minWidth: 275 }}>
-          <CardContent>
-            <Typography
-              sx={{ fontSize: 14 }}
-              color="text.secondary"
-              gutterBottom
-            >
-              Word of the Day
-            </Typography>
-            <Typography variant="h5" component="div">
-              Hello
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              adjective
-            </Typography>
-            <Typography variant="body2">
-              well meaning and kindly.
-              <br />
-              {'"a benevolent smile"'}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Learn More</Button>
-          </CardActions>
-        </Card>
-
-        <ListItemButton component="a" href="#simple-list">
-          <ListItemText primary="Spam" />
-        </ListItemButton>
-
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={8}>
-              <Paper>xs=8</Paper>
-            </Grid>
-            <Grid item xs={4}>
-              <Paper>xs=4</Paper>
-            </Grid>
-            <Grid item xs={4}>
-              <Paper>xs=4</Paper>
-            </Grid>
-            <Grid item xs={8}>
-              <Paper>xs=8</Paper>
-            </Grid>
-          </Grid>
-        </Box>
+        <div>
+          <span>With default Theme:</span>
+        </div>
+        <Switch {...label} defaultChecked />
+        <Switch {...label} />
+        <Switch {...label} disabled defaultChecked />
       </main>
     </>
   );
